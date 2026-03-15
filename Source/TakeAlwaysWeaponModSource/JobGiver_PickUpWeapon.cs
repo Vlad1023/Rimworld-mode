4da3cc3879c51fb05c2e53c99aed23a039da19e9
Vlad <volkovskiy4@gmail.com>
2026-03-15 20:34:57 +0100

    fixed spacing

--- Source/TakeAlwaysWeaponModSource/JobGiver_PickUpWeapon.cs
+++ Source/TakeAlwaysWeaponModSource/JobGiver_PickUpWeapon.cs
@@ -7,115 +7,114 @@ using Verse.AI;
 namespace Rimworld
 {
     public class JobGiver_PickUpWeapon : ThinkNode_JobGiver
-    {
+    {    
+        private bool preferBuildingDestroyers;
+        private bool pickUpUtilityItems;
       
-      private bool preferBuildingDestroyers;
-      private bool pickUpUtilityItems;
-      
-      private static Dictionary<Pawn, int> lastJobTick = new Dictionary<Pawn, int>();
+        private static Dictionary<Pawn, int> lastJobTick = new Dictionary<Pawn, int>();
 
-      private float MinMeleeWeaponDPSThreshold
-      {
-        get
+        private float MinMeleeWeaponDPSThreshold
         {
-          List<Tool> tools = ThingDefOf.Human.tools;
-          float num = 0.0f;
-          for (int index = 0; index < tools.Count; ++index)
-          {
-            if (tools[index].linkedBodyPartsGroup == BodyPartGroupDefOf.LeftHand || tools[index].linkedBodyPartsGroup == BodyPartGroupDefOf.RightHand)
+            get
             {
-              num = tools[index].power / tools[index].cooldownTime;
-              break;
+                List<Tool> tools = ThingDefOf.Human.tools;
+                float num = 0.0f;
+                for (int index = 0; index < tools.Count; ++index)
+                {
+                if (tools[index].linkedBodyPartsGroup == BodyPartGroupDefOf.LeftHand || tools[index].linkedBodyPartsGroup == BodyPartGroupDefOf.RightHand)
+                {
+                    num = tools[index].power / tools[index].cooldownTime;
+                    break;
+                }
+                }
+                return num + 2f;
             }
-          }
-          return num + 2f;
         }
-      }
 
-      public override ThinkNode DeepCopy(bool resolve = true)
-      {
-        JobGiver_PickUpWeapon opportunisticWeapon = (JobGiver_PickUpWeapon) base.DeepCopy(resolve);
-        opportunisticWeapon.preferBuildingDestroyers = this.preferBuildingDestroyers;
-        opportunisticWeapon.pickUpUtilityItems = this.pickUpUtilityItems;
-        return (ThinkNode) opportunisticWeapon;
-      }
+        public override ThinkNode DeepCopy(bool resolve = true)
+        {
+            JobGiver_PickUpWeapon opportunisticWeapon = (JobGiver_PickUpWeapon) base.DeepCopy(resolve);
+            opportunisticWeapon.preferBuildingDestroyers = this.preferBuildingDestroyers;
+            opportunisticWeapon.pickUpUtilityItems = this.pickUpUtilityItems;
+            return (ThinkNode) opportunisticWeapon;
+        }
       
-      protected override Job TryGiveJob(Pawn pawn)
-      {
-        int currentTick = Find.TickManager.TicksGame;
-        if (lastJobTick.ContainsKey(pawn) && currentTick - lastJobTick[pawn] < 100)
+        protected override Job TryGiveJob(Pawn pawn)
         {
-          return null; // Too soon to assign any of the followed jobs again
-        }
-        if ((pawn.equipment == null && pawn.apparel == null) || pawn.CurJob?.def == JobDefOfEquipWeapon.EquipWeapon)
-          return (Job) null;
-        if (pawn.RaceProps.Humanlike && pawn.WorkTagIsDisabled(WorkTags.Violent))
-          return (Job) null;
-        if (!pawn.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation))
-          return (Job) null;
-        if (pawn.GetRegion() == null)
-          return (Job) null;
-        if (pawn.equipment != null && !this.AlreadySatisfiedWithCurrentWeapon(pawn))
-        {
-          Thing targetA = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Weapon), PathEndMode.Touch, TraverseParms.For(pawn), 500f, (Predicate<Thing>) (x => pawn.CanReserve((LocalTargetInfo) x) && !x.IsBurning() && this.ShouldEquipWeapon(x, pawn)));
-          if (targetA != null)
-          {
-            pawn.playerSettings.hostilityResponse = HostilityResponseMode.Attack; // so pawn is able to attack but it just a question of finding weapon.
-            var job = JobMaker.MakeJob(JobDefOfEquipWeapon.EquipWeapon, (LocalTargetInfo)targetA);
-            if (job != null)
+            int currentTick = Find.TickManager.TicksGame;
+            if (lastJobTick.ContainsKey(pawn) && currentTick - lastJobTick[pawn] < 100)
             {
-              lastJobTick[pawn] = currentTick; // Record when the job was assigned
+                return null; // Too soon to assign any of the followed jobs again
             }
-            return job;
-          }
-          if (pawn.equipment.Primary != null && !SlaveRebellionUtility.WeaponUsableInRebellion((Thing) pawn.equipment.Primary)) // would leave it here but I suppose might be removed
-            return JobMaker.MakeJob(JobDefOf.DropEquipment, (LocalTargetInfo) (Thing) pawn.equipment.Primary);
-        }
-        Pawn_EquipmentTracker equipment = pawn.equipment;
-        int num;
-        if (equipment == null)
-        {
-          num = 0;
-        }
-        else
-        {
-          bool? isRangedWeapon = equipment.Primary?.def?.IsRangedWeapon;
-          bool flag = true;
-          num = isRangedWeapon.GetValueOrDefault() == flag & isRangedWeapon.HasValue ? 1 : 0;
-        }
-        if (num != 0)
-        {
-          foreach (Apparel targetA in pawn.apparel.WornApparel)
-          {
-            if (targetA.def == ThingDefOf.Apparel_ShieldBelt)
+            if ((pawn.equipment == null && pawn.apparel == null) || pawn.CurJob?.def == JobDefOfEquipWeapon.EquipWeapon)
+                return (Job) null;
+            if (pawn.RaceProps.Humanlike && pawn.WorkTagIsDisabled(WorkTags.Violent))
+                return (Job) null;
+            if (!pawn.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation))
+                return (Job) null;
+            if (pawn.GetRegion() == null)
+                return (Job) null;
+            if (pawn.equipment != null && !this.AlreadySatisfiedWithCurrentWeapon(pawn))
             {
-              return JobMaker.MakeJob(JobDefOf.RemoveApparel, (LocalTargetInfo) (Thing) targetA);
+                Thing targetA = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Weapon), PathEndMode.Touch, TraverseParms.For(pawn), 500f, (Predicate<Thing>) (x => pawn.CanReserve((LocalTargetInfo) x) && !x.IsBurning() && this.ShouldEquipWeapon(x, pawn)));
+                if (targetA != null)
+                {
+                pawn.playerSettings.hostilityResponse = HostilityResponseMode.Attack; // so pawn is able to attack but it just a question of finding weapon.
+                var job = JobMaker.MakeJob(JobDefOfEquipWeapon.EquipWeapon, (LocalTargetInfo)targetA);
+                if (job != null)
+                {
+                    lastJobTick[pawn] = currentTick; // Record when the job was assigned
+                }
+                return job;
+                }
+                if (pawn.equipment.Primary != null && !SlaveRebellionUtility.WeaponUsableInRebellion((Thing) pawn.equipment.Primary)) // would leave it here but I suppose might be removed
+                return JobMaker.MakeJob(JobDefOf.DropEquipment, (LocalTargetInfo) (Thing) pawn.equipment.Primary);
             }
-          }
+            Pawn_EquipmentTracker equipment = pawn.equipment;
+            int num;
+            if (equipment == null)
+            {
+                num = 0;
+            }
+            else
+            {
+                bool? isRangedWeapon = equipment.Primary?.def?.IsRangedWeapon;
+                bool flag = true;
+                num = isRangedWeapon.GetValueOrDefault() == flag & isRangedWeapon.HasValue ? 1 : 0;
+            }
+            if (num != 0)
+            {
+                foreach (Apparel targetA in pawn.apparel.WornApparel)
+                {
+                if (targetA.def == ThingDefOf.Apparel_ShieldBelt)
+                {
+                    return JobMaker.MakeJob(JobDefOf.RemoveApparel, (LocalTargetInfo) (Thing) targetA);
+                }
+                }
+            }
+            if (this.pickUpUtilityItems && pawn.apparel != null && this.WouldPickupUtilityItem(pawn))
+            {
+                Thing targetA = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Apparel), PathEndMode.OnCell, TraverseParms.For(pawn), 8f, (Predicate<Thing>) (x => pawn.CanReserve((LocalTargetInfo) x) && !x.IsBurning() && this.ShouldEquipUtilityItem(x, pawn)), searchRegionsMax: 15);
+                if (targetA != null)
+                return JobMaker.MakeJob(JobDefOf.Wear, (LocalTargetInfo) targetA);
+            }
+            return (Job) null;
         }
-        if (this.pickUpUtilityItems && pawn.apparel != null && this.WouldPickupUtilityItem(pawn))
-        {
-          Thing targetA = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Apparel), PathEndMode.OnCell, TraverseParms.For(pawn), 8f, (Predicate<Thing>) (x => pawn.CanReserve((LocalTargetInfo) x) && !x.IsBurning() && this.ShouldEquipUtilityItem(x, pawn)), searchRegionsMax: 15);
-          if (targetA != null)
-            return JobMaker.MakeJob(JobDefOf.Wear, (LocalTargetInfo) targetA);
-        }
-        return (Job) null;
-      }
       
-      private bool AlreadySatisfiedWithCurrentWeapon(Pawn pawn)
-      {
-        ThingWithComps primary = pawn.equipment.Primary;
-        if (primary == null)
-          return false;
-        if (this.preferBuildingDestroyers)
+        private bool AlreadySatisfiedWithCurrentWeapon(Pawn pawn)
         {
-          if (!pawn.equipment.PrimaryEq.PrimaryVerb.verbProps.ai_IsBuildingDestroyer)
-            return false;
+            ThingWithComps primary = pawn.equipment.Primary;
+            if (primary == null)
+                return false;
+            if (this.preferBuildingDestroyers)
+            {
+                if (!pawn.equipment.PrimaryEq.PrimaryVerb.verbProps.ai_IsBuildingDestroyer)
+                return false;
+            }
+            else if (!SlaveRebellionUtility.WeaponUsableInRebellion((Thing) primary)) // would leave it here but I suppose might be removed
+                return false;
+            return true;
         }
-        else if (!SlaveRebellionUtility.WeaponUsableInRebellion((Thing) primary)) // would leave it here but I suppose might be removed
-          return false;
-        return true;
-      }
 
         private bool ShouldEquipWeapon(Thing newWep, Pawn pawn)
         {
@@ -145,15 +144,15 @@ namespace Rimworld
             return wep.def.IsRangedWeapon ? 2 : 1;
         }
 
-      private bool WouldPickupUtilityItem(Pawn pawn)
-      {
-        return pawn.equipment?.Primary == null && pawn.apparel.FirstApparelVerb == null;
-      }
+        private bool WouldPickupUtilityItem(Pawn pawn)
+        {
+            return pawn.equipment?.Primary == null && pawn.apparel.FirstApparelVerb == null;
+        }
 
-      private bool ShouldEquipUtilityItem(Thing thing, Pawn pawn)
-      {
-        return thing is Apparel newApparel && newApparel.def.apparel.ai_pickUpOpportunistically && EquipmentUtility.CanEquip((Thing) newApparel, pawn) && ApparelUtility.HasPartsToWear(pawn, newApparel.def) && !pawn.apparel.WouldReplaceLockedApparel(newApparel);
-      }
+        private bool ShouldEquipUtilityItem(Thing thing, Pawn pawn)
+        {
+            return thing is Apparel newApparel && newApparel.def.apparel.ai_pickUpOpportunistically && EquipmentUtility.CanEquip((Thing) newApparel, pawn) && ApparelUtility.HasPartsToWear(pawn, newApparel.def) && !pawn.apparel.WouldReplaceLockedApparel(newApparel);
+        }
       
     }
 }
